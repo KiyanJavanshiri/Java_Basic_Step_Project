dae47a7b22cb0902c62b4c844bfe2be985764e1a
YaroslavHavrylenko <yasx80@gmail.com>
2025-11-26 10:44:50 +0200

    Flight - updateFlight method fix issues

--- src/main/java/flightreservation/Main.java
+++ src/main/java/flightreservation/Main.java
@@ -16,21 +16,21 @@ public class Main {
         Flight flight = new Flight("RND123", Destination.BEIJING, LocalDate.now(), LocalTime.now(),10);
         System.out.println(flight);
 
-        FlightsController flightsController = new FlightsController();
+//        FlightsController flightsController = new FlightsController();
 
 //        List<Flight> flights = flightsController.generateListOfFlight();
-        if (flightsController.loadFlightsFromDB()){
-            flightsController.displayFlightsForToday();
-        }
+//        if (flightsController.loadFlightsFromDB()){
+//            flightsController.displayFlightsForToday();
+//        }
 //        List<Flight> flights = flightsController.loadFlightsFromDB();
-        flightsController.displayFlightsForToday();
-        flightsController.getAllFlights();
-
-        System.out.println(flightsController.getFlightById("FL20000"));
-
-        List<Flight> searchFlights = flightsController.searchFlights("Seoul", LocalDate.of(2025,12, 10), 3);
-        System.out.println(searchFlights);
-        flightsController.updateFlight("FL0040", 3);
-        System.out.println(flightsController.getFlightById("FL0040"));
+//        flightsController.displayFlightsForToday();
+//        flightsController.getAllFlights();
+//
+//        System.out.println(flightsController.getFlightById("FL20000"));
+//
+//        List<Flight> searchFlights = flightsController.searchFlights("Seoul", LocalDate.of(2025,12, 10), 3);
+//        System.out.println(searchFlights);
+//        flightsController.updateFlight("FL0040", 3);
+//        System.out.println(flightsController.getFlightById("FL0040"));
     }
 }
