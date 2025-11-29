607d71dd5c82897b59e322a9a127fdca5d97a994
timurgogogo-ux <timurgogogo@gmail.com>
2025-11-29 11:12:43 +0200

    New console test (#6)
    * add Console
    
    * Console_don

--- src/main/java/flightreservation/Main.java
+++ src/main/java/flightreservation/Main.java
@@ -11,27 +11,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-
-
-        Flight flight = new Flight("RND123", Destination.BEIJING, LocalDate.now(), LocalTime.now(),10);
-        System.out.println(flight);
-
-//        FlightsController flightsController = new FlightsController();
-
-//        List<Flight> flights = flightsController.generateListOfFlight();
-//        if (flightsController.loadFlightsFromDB()){
-//            flightsController.displayFlightsForToday();
-//        }
-//        List<Flight> flights = flightsController.loadFlightsFromDB();
-//        flightsController.displayFlightsForToday();
-//        flightsController.getAllFlights();
-//
-//        System.out.println(flightsController.getFlightById("FL20000"));
-//
-//        List<Flight> searchFlights = flightsController.searchFlights("Seoul", LocalDate.of(2025,12, 10), 3);
-//        System.out.println(searchFlights);
-//        flightsController.updateFlight("FL0040", 3);
-//        System.out.println(flightsController.getFlightById("FL0040"));
-//        bookingController.saveToFile();
+        ApplicationConsole applicationConsole = new ApplicationConsole();
+        applicationConsole.start();
     }
 }
